e3393778067023e74fc68051f2767c48bdaccaed
purhan <official.purhan@gmail.com>
2020-08-29 09:21:29 +0530

    add README.md

--- assistant.sh
+++ assistant.sh
@@ -37,7 +37,7 @@ do
             -a)
                 echo "Adding the whole config ..."
                 cd ~/
-                config add .gitignore assistant.sh .bashrc .vimrc .astylerc conf-scripts .gtkrc-2.0 RICE vampire.sh
+                config add README.md .gitignore assistant.sh .bashrc .vimrc .astylerc conf-scripts .gtkrc-2.0 RICE vampire.sh
 
                 cd .config
                 config add awesome Code sublime-text-3 gtk-2.0 gtk-3.0 guake powerline-shell konsolerc spectaclerc termite
